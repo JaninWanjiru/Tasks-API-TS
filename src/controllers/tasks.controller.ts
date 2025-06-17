4d4a23752326202e066f0d89afada2b0413ff9f8
JaninWanjiru <janinndongo02@gmail.com>
2025-06-17 16:14:55 +0300

    add a route to create a task

--- src/controllers/tasks.controller.ts
+++ src/controllers/tasks.controller.ts
@@ -1,12 +1,31 @@
 import {Request, Response} from 'express';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Task } from '@prisma/client';
 const client = new PrismaClient();
 
+// Get all tasks
 export const getAllTasks = async (_req: Request, res: Response) => {
   try {
     const tasks = await client.task.findMany();
     res.status(200).json(tasks);
   } catch (_e) {
     res.status(500).json({ message: "Something went wrong" });
+  } 
+}
+
+// Create a task
+export const createTask = async (req: Request, res: Response) => {
+  try {
+    const { title, description }: Task = req.body;
+    const newTask = await client.task.create({
+      data: {
+        title,
+        description
+      }
+    });
+    res.status(201).json(newTask);
+  } catch (_e) {
+    res.status(500).json({ message: "Something went wrong" });
   }
 }
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import express from 'express';
-import { getAllTasks } from './controllers/tasks.controller';
+import { createTask, getAllTasks } from './controllers/tasks.controller';
 
 const app = express()
+
+app.use(express.json())
 
 app.get("/", (_req, res) => {
     res.send("<h1>You've unlocked the Tasks API in Ts and Express</h1> ")
 })
 
-// Get all tasks
 app.get("/tasks", getAllTasks);
+app.post("/tasks", createTask);
 
 const port = process.env.PORT || 5500;
 app.listen(port, () => console.log(`App running on port ${port}`));
