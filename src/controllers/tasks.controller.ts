391411cbd50711c77a58025722aed7e56808a2ed
JaninWanjiru <janinndongo02@gmail.com>
2025-06-17 17:44:47 +0300

    add a route to get specific task using Id

--- src/controllers/tasks.controller.ts
+++ src/controllers/tasks.controller.ts
@@ -1,5 +1,5 @@
-import {Request, Response} from 'express';
-import { PrismaClient, Task } from '@prisma/client';
+import { Request, Response } from "express";
+import { PrismaClient, Task } from "@prisma/client";
 const client = new PrismaClient();
 
 // Get all tasks
@@ -9,8 +9,8 @@ export const getAllTasks = async (_req: Request, res: Response) => {
     res.status(200).json(tasks);
   } catch (_e) {
     res.status(500).json({ message: "Something went wrong" });
-  } 
-}
+  }
+};
 
 // Create a task
 export const createTask = async (req: Request, res: Response) => {
@@ -19,13 +19,26 @@ export const createTask = async (req: Request, res: Response) => {
     const newTask = await client.task.create({
       data: {
         title,
-        description
-      }
+        description,
+      },
     });
     res.status(201).json(newTask);
   } catch (_e) {
     res.status(500).json({ message: "Something went wrong" });
   }
-}
+};
 
-
+// Get a specific task with Id
+export const getSpecificTask = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const specificTask = await client.task.findFirst({
+      where: {
+        id,
+      },
+    });
+    res.status(200).json(specificTask);
+  } catch (e) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
-import express from 'express';
-import { createTask, getAllTasks } from './controllers/tasks.controller';
+import express from "express";
+import {
+  createTask,
+  getAllTasks,
+  getSpecificTask,
+} from "./controllers/tasks.controller";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", (_req, res) => {
-    res.send("<h1>You've unlocked the Tasks API in Ts and Express</h1> ")
-})
+  res.send("<h1>You've unlocked the Tasks API in Ts and Express</h1> ");
+});
 
 app.get("/tasks", getAllTasks);
 app.post("/tasks", createTask);
+app.get("/tasks/:id", getSpecificTask);
 
 const port = process.env.PORT || 5500;
 app.listen(port, () => console.log(`App running on port ${port}`));
